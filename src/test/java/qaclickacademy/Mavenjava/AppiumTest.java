ac836fd36449fa5726578da53821f56795bf92f4
shreeshankari <shankarishree96@gmail.com>
2022-07-13 09:35:30 +0530

    Commit message

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+		@Test
+		public void NativeAppAndriod() {
+		System.out.println("NativeAppAndriod");
+		}
+		
+		@Test
+	public void IOSApps() {
+			System.out.println("IOSApps");
+		}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+//trigger testng xml from maven
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+public void elementUI() {
+		System.out.println("elementUI");
+	}
+}
